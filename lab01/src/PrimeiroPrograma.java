673021c8e0967e286a1f357edcc381c230f87753
Winicius Allan <winiciusab12@gmail.com>
2023-03-21 13:21:53 -0300

    Adiciona questao

---
+++ lab01/src/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+* Laboratório de Programação - Lab 1
+*
+* @author Winicius Allan - 122110397
+*/
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        // int gastoGasolina = 140;
+        // int dinheiro = 500;
+        // dinheiro -= gastoGasolina;
+        // dinheiro -= gastoGasolina;
+
+        int saida = 42;
+        System.out.println(saida);
+
+        input.close();
+    }
+}
